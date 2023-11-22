ed7fff8acf9a26a64a98d0727ba1619b870fa41d
secona <77039267+secona@users.noreply.github.com>
2023-11-22 16:11:11 +0700

    inputs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,16 +2,16 @@ export default function Login() {
   return (
     <div className="mx-auto min-h-screen flex flex-col gap-2 items-center justify-center w-full">
       <form className="border-white/10 border-2 border-solid rounded-lg py-12 px-20 flex flex-col gap-2 justify-center">
-        <p className="text-7xl font-clash-display mb-12 text-center">Login</p>
+        <p className="text-7xl font-clash-display mb-5 text-center">Login</p>
         <input
-          className="rounded-lg py-2 px-3 text-black"
+          className="rounded-lg py-2 px-3 text-black bg-transparent border-white/10 border-2 border-solid"
           placeholder="Email"
         />
         <input
-          className="rounded-lg py-2 px-3 text-black"
+          className="rounded-lg py-2 px-3 text-black bg-transparent border-white/10 border-2 border-solid"
           placeholder="Password"
         />
-        <button className="bg-blue-600 py-2 px-6 rounded-lg">Log In</button>
+        <button className="bg-blue-600 py-2 px-6 rounded-lg mt-2">Log In</button>
       </form>
     </div>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,20 +2,20 @@ export default function Signup() {
   return (
     <div className="mx-auto min-h-screen flex flex-col gap-2 items-center justify-center w-full">
       <form className="border-white/10 border-2 border-solid rounded-lg py-12 px-20 flex flex-col gap-2 justify-center">
-        <p className="text-7xl font-clash-display mb-12 text-center">Sign Up</p>
+        <p className="text-7xl font-clash-display mb-5 text-center">Sign Up</p>
         <input
-          className="rounded-lg py-2 px-3 text-black"
+          className="rounded-lg py-2 px-3 text-black bg-transparent border-white/10 border-2 border-solid"
           placeholder="Email"
         />
         <input
-          className="rounded-lg py-2 px-3 text-black"
+          className="rounded-lg py-2 px-3 text-black bg-transparent border-white/10 border-2 border-solid"
           placeholder="Name"
         />
         <input
-          className="rounded-lg py-2 px-3 text-black"
+          className="rounded-lg py-2 px-3 text-black bg-transparent border-white/10 border-2 border-solid"
           placeholder="Password"
         />
-        <button className="bg-blue-600 py-2 px-6 rounded-lg">Log In</button>
+        <button className="bg-blue-600 py-2 px-6 rounded-lg mt-2">Log In</button>
       </form>
     </div>
   );
