41f419608eb2a18abba49459adb8e84a0124757b
secona <77039267+secona@users.noreply.github.com>
2023-11-22 14:32:32 +0700

    signup and login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,18 @@
 export default function Login() {
   return (
-    <h1>Login</h1>
-  )
+    <div className="mx-auto min-h-screen flex flex-col gap-2 items-center justify-center w-full">
+      <form className="border-white/10 border-2 border-solid rounded-lg p-10 flex flex-col gap-2 justify-center">
+        <p className="text-4xl mb-6 text-center">Login</p>
+        <input
+          className="rounded-lg py-2 px-3 text-black"
+          placeholder="Email"
+        />
+        <input
+          className="rounded-lg py-2 px-3 text-black"
+          placeholder="Password"
+        />
+        <button className="bg-blue-600 py-2 px-6 rounded-lg">Log In</button>
+      </form>
+    </div>
+  );
 }
